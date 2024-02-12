f9f6b12426dc18a309b176d25b51d851fe0d9241
Maxime Tournier <mxm.tournier@gmail.com>
2024-02-12 15:49:11 +0100

    first commit

---
+++ overlay/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Overlays</title>
+</head>
+<body>
+<main>
+
+
+
+</main>
+
+
+
+<style>
+
+
+
+    main{
+
+        width: 800px;
+        height: 600px;
+        background-color: red;
+
+    }
+
+
+
+</style>
+
+</body>
+</html>
